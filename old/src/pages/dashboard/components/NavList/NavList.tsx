575f2b375cc0b4c153b139358ab771c5fb49623c
Ondřej Tuček <ondrejtucek9@gmail.com>
2023-01-07 11:27:17 +0100

    Client rework prepare
    I would like to rework how client works (totally)
    
    - I wanna distch CRA in favor of Vite
    
    - I also wanna implement redux
    
    - The client needs to look profesional

--- old/src/pages/dashboard/components/NavList/NavList.tsx
+++ old/src/pages/dashboard/components/NavList/NavList.tsx
@@ -1,6 +1,6 @@
 import { Typography } from '@material-tailwind/react';
 import { Link } from 'react-router-dom';
-import { ulOptions } from '../.././../../data/sidebar';
+import { ulOptions } from '../../../../data/sidebar';
 
 const NavList = () => (
 	<ul className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
